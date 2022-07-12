03d15dcb3890e0d35fbb695f0106fedf66115899
R·E <97787451+I-am-R-E@users.noreply.github.com>
2022-07-12 17:10:08 +0800

    Update CaiXin.js

--- CaiXin/Script/CaiXin.js
+++ CaiXin/Script/CaiXin.js
@@ -1,2 +1,3 @@
-//new way
-var _0x4a8488=$request['headers'];const _0x5dec9b=$request['url']['replace'](/uid=(\d+|)/g,'uid=12781895')['replace'](/code=(\w+|)/g,'code=346E970A25AB143AD09D91FED6F9CA0C')['replace'](/device=(\w+|)/g,'device=699440d159f8b18216b678d3261c91afa3a8a444')['replace'](/deviceType=(\d+|)/g,'deviceType=1');$done({'url':_0x5dec9b,'headers':_0x4a8488});
+//财新解锁阅读限制
+//随时拉闸
+var _0x2435d8=$request['headers'];_0x2435d8['User-Agent']='Mozilla/5.0\x20(iPhone;\x20CPU\x20iPhone\x20OS\x2015_4_1\x20like\x20Mac\x20OS\x20X)\x20AppleWebKit/605.1.15\x20(KHTML,\x20like\x20Gecko)\x20Mobile/15E148\x20CaixinV5/7.9.3\x20deviceType/1';const _0x4f2de0=$request['url']['replace'](/uid=(\d+|)/g,'uid=12781895')['replace'](/code=(\w+|)/g,'code=346E970A25AB143AD09D91FED6F9CA0C')['replace'](/device=(\w+|)/g,'device=699440d159f8b18216b678d3261c91afa3a8a444')['replace'](/deviceType=(\d+|)/g,'deviceType=1');$done({'url':_0x4f2de0,'headers':_0x2435d8});
