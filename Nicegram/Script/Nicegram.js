6d72d54ef21c702ca907db378dd3cba9fce680be
R·E <97787451+I-am-R-E@users.noreply.github.com>
2022-07-15 00:51:53 +0800

    Create Nicegram.js

---
+++ Nicegram/Script/Nicegram.js
@@ -0,0 +1,9 @@
+/*
+脚本功能：解锁Nicegram会员
+脚本作者：R·E
+支持版本：商店最新 1.0.7
+更新时间：2022.07.15
+问题反馈：https://t.me/Functional_Store_Hub
+使用声明：⚠️⚠️⚠️此脚本仅供学习与交流，禁止转载与贩卖！⚠️⚠️⚠️
+*/
+$done({'status':'200','headers':{'Content-Type':'application/json'},'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'});
