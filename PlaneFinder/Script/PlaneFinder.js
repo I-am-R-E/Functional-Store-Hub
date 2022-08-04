fbb8b98499d19a74b4c8fc43adab512f71016d8c
R·E <97787451+I-am-R-E@users.noreply.github.com>
2022-08-04 22:49:36 +0800

    Add files via upload

---
+++ PlaneFinder/Script/PlaneFinder.js
@@ -0,0 +1,13 @@
+/*
+脚本功能：解锁PlaneFinder高级订阅
+脚本作者：R·E
+脚本发布：https://t.me/Functional_Store_Hub
+
+更新时间：2022.08.04
+支持版本：商店版本 
+下载地址：https://apps.apple.com/app/id361273585
+
+使用声明：⚠️⚠️⚠️此脚本仅供学习与交流,禁止转载与贩卖,需在24小时内从设备中删除！⚠️⚠️⚠️
+*/
+
+var _0x5ec833=JSON['parse']($response['body']);var _0x430258={};_0x430258['type']='itunes';_0x430258['manageURL']='itms-apps://apps.apple.com/account/subscriptions';_0x430258['expiryTS']=0x70db6800;_0x430258['renews']=!![];_0x5ec833['payload']['subscription']=_0x430258;_0x5ec833['payload']['permissions']=[0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xf,0x10];var _0x4cb17f={};_0x4cb17f['body']=JSON['stringify'](_0x5ec833);$done(_0x4cb17f);
